620f6191caa7f30c21c7d38668d08ad8c789fb33
SageScroll18144 <ffss@cin.ufpe.br>
2023-01-31 21:19:00 -0300

    menu implementado

--- src/map_csv.c
+++ src/map_csv.c
@@ -4,17 +4,24 @@
 
 int main(){
 
-    FILE *file_writer = fopen("teste2.csv", "a");  
+    char file_name[50];
+    printf("Provide the project (.qsf) name.: ");
+    scanf(" %s", file_name);
+
+    char path[110];
+    sprintf(path, "%s.csv", file_name);
+
+    FILE *file_writer = fopen(path, "a");  
     FILE *file_reader = fopen("pins.txt", "r");
     
-    fwrite("\n", sizeof(char), 1, file_writer);
-    char tmp[110];
+    //fwrite("\n", sizeof(char), 1, file_writer);
+    char line[110];
     while(!feof(file_reader)){
-        fgets(tmp, 110, file_reader);
-        printf("%s", tmp);
-        fwrite(tmp, sizeof(char), strlen(tmp), file_writer);
+        fgets(line, 110, file_reader);
+        printf("%s", line);
+        fwrite(line, sizeof(char), strlen(line), file_writer);
     }
-    fwrite("\n", sizeof(char), 1, file_writer);
+    //fwrite("\n", sizeof(char), 1, file_writer);
 
     fclose(file_writer);
     fclose(file_reader);
